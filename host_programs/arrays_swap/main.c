8bcd7bffbe40c88f05b7308afb7e5f10b67a8f2f
davidjmoloney <davidjkmoloney@gmail.com>
2022-03-27 15:14:46 +0100

    Commit-27Mar22-Array_swap+Unions

--- host_programs/arrays_swap/main.c
+++ host_programs/arrays_swap/main.c
@@ -1,16 +1,24 @@
 
 
+/*
+ * Code designed to take two arrays of integers from user and swap contents between the arrays
+ */
+
+
 
 #include<stdio.h>
 #include<stdint.h>
 
+
+//Prototyps for functions defined at the end of the program code
 void wait_for_user_input(void);
-void display_array(int32_t *pArray1,uint32_t nitem);
+void array_input(int32_t *pArray, uint32_t nitem);
+void display_array(int32_t *pArray,uint32_t nitem);
 void swap_arrays(int32_t *array1, int32_t *array2, uint32_t nitem1, uint32_t nitem2);
 
 int main(void)
 {
-    int32_t nItem1, nItem2;
+    int32_t nItem1, nItem2;		//length of each array
     printf("Array swapping program\n");
     printf("Enter no of elements of Array-1 and Array-2:");
     scanf("%d %d",&nItem1,&nItem2);
@@ -19,24 +27,22 @@ int main(void)
     {
     	printf("Number of elements cannot be negative\n");
     	wait_for_user_input();
-    	return 0;
+    	return 0;		//Finish code if one of the two arrays has been given a negative size
     }
 
+
+    //Define the arrays
     int32_t array1[nItem1];
     int32_t array2[nItem2];
 
-    for(uint32_t i = 0; i < nItem1 ; i++ )
-    {
-    	printf("Enter %d element of array1:",i);
-    	scanf("%d",&array1[i]);
-    }
 
-    for(uint32_t i = 0; i < nItem2 ; i++ )
-    {
-    	printf("Enter %d element of array2:",i);
-    	scanf("%d",&array2[i]);
-    }
+    //Take input from user
+    array_input(array1, nItem1);
 
+    array_input(array2, nItem2);
+
+
+    //Display arrays
 	printf("Contents of arrays before swap \n");
 
 	display_array(array1,nItem1);
@@ -47,9 +53,13 @@ int main(void)
 
 	printf("\n");
 
-	printf("Contents of arrays after swap\n");
 
+	//Swap Contents
 	swap_arrays(array1,array2,nItem1,nItem2);
+
+
+	//Display again
+	printf("Contents of arrays after swap\n");
 
 	display_array(array1,nItem1);
 
@@ -59,6 +69,7 @@ int main(void)
 
 	printf("\n");
 
+	//Finish
 	wait_for_user_input();
 
 	return 0;
@@ -66,18 +77,28 @@ int main(void)
 
 }
 
+//We pass array to the function by using the pointer to its first element
+void array_input(int32_t *pArray, uint32_t nitem){
+	 for(uint32_t i = 0; i < nitem ; i++ ){
+		 	 printf("Enter %d element of array1:",i);
+		 	 scanf("%d",&pArray[i]);	//here we are effectively dereferencing pointer using the '[]' syntax.
+		 	 	 	 	 	 	 	 	//We are then writing  to the address of this variable with '&'.
+		 	 //scanf("%d",pArray + i);  <-- This line will give the exact same result - here we just use pointer
+	 }
 
-void display_array(int32_t *pArray1,uint32_t nitem)
-{
+}
+
+void display_array(int32_t *pArray,uint32_t nitem){
+
 	for(uint32_t i = 0 ; i < nitem ; i++)
 	{
-		printf("%4d  ",pArray1[i]);
+		printf("%4d  ",pArray[i]);
 	}
 }
 
 
-void swap_arrays(int32_t *array1, int32_t *array2, uint32_t nitem1, uint32_t nitem2)
-{
+void swap_arrays(int32_t *array1, int32_t *array2, uint32_t nitem1, uint32_t nitem2){
+
    uint32_t len = nitem1 < nitem2 ? nitem1 : nitem2;
 
    for(uint32_t i = 0 ; i < len ; i++)
@@ -90,8 +111,7 @@ void swap_arrays(int32_t *array1, int32_t *array2, uint32_t nitem1, uint32_t nit
 }
 
 
-void wait_for_user_input(void)
-{
+void wait_for_user_input(void){
 
 	printf("Press enter key to exit this application");
 
--- host_programs/unionBitExraction/main.c
+++ host_programs/unionBitExraction/main.c
@@ -3,10 +3,18 @@
 #include<stdint.h>
 #include<stdio.h>
 
+
 /*
- * This structure we used to store various fields of the packet in to variables
- * The variable of this structure consumes 4 bytes in the memory
-*/
+ * Here we use a clever trick of c to quickly write to entities of struct variable
+ * For this the variables must be allocated with bitfields
+ */
+
+
+//A union will only use as much memory as its longest part -
+// - in this case it uses 32 bits as packetValue and the struct packetFields both use 32 bits
+// - if we write our 32 bit input packet to the union as packetValue this will write directly into all 32bits of the memory
+// - we can then access the bitfields using their relative names as defined in the struct section of the union
+
 union Packet
 {
 	uint32_t packetValue;
@@ -21,7 +29,7 @@ union Packet
 		uint32_t longAddr		:8;
 		uint32_t shortAddr		:2;
 		uint32_t addrMode		:1;
-	}packetFields;
+	}packetFields;	//because this is a nested struct we can define it like this - its name won't change or be defined elsewhere
 
 };
 
@@ -31,9 +39,10 @@ int main(void)
 
    union Packet packet;
    printf("Enter the 32bit packet value:");
-   scanf("%X",&packet.packetValue);
+   scanf("%X",&packet.packetValue);			//takes input from user
 
-   printf("crc      :%#x\n",packet.packetFields.crc);
+   //we can print each sub-entity of the struct within the union like this
+   printf("crc      :%#x\n",packet.packetFields.crc);	//prints crc of the packetField struct within the packet Union
    printf("status   :%#x\n",packet.packetFields.status);
    printf("payload  :%#x\n",packet.packetFields.payload);
    printf("bat      :%#x\n",packet.packetFields.bat);
@@ -49,7 +58,7 @@ int main(void)
    while(getchar() != '\n');
    getchar();
 
-	return 0;
+   return 0;
 
 }
 
